5ee9755c949a59d5cd2a16de6312a349438db9ac
bayousoft <bayousoft@gmail.com>
2011-10-08 17:14:57 -0500

    First commit
    First commit to Github

---
+++ page.tpl.php
@@ -0,0 +1,60 @@
+<div id="wrapper" class="clearfix">
+
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    <?php if ($main_menu): ?>
+      <a href="#navigation" class="element-invisible element-focusable"><?php print t('Skip to navigation'); ?></a>
+    <?php endif; ?>
+  </div>
+
+  <header id="header" role="banner" class="clearfix">
+	<?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" id="logo">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+    <?php endif; ?>
+    <?php print render($page['header']); ?>
+  </header> <!-- /#header -->
+  <?php if ($main_menu || $secondary_menu || $page['navigation']): ?>
+    <nav id="navigation" role="navigation" class="clearfix">
+      <?php if ($page['navigation']): ?> <!--if block in navigation region, override $main_menu and $secondary_menu-->
+        <?php print render($page['navigation']); ?>
+      <?php endif; ?>
+      <?php if (!$page['navigation']): ?>
+        <?php if ($main_menu): print $main_menu; endif; ?>
+        <?php //if ($secondary_menu): print $secondary_menu; endif; ?>
+      <?php endif; ?>
+    </nav> <!-- /#navigation -->
+  <?php endif; ?>
+  <?php if ($breadcrumb): print $breadcrumb; endif;?>  
+  <section id="main" role="main" class="clearfix">
+    <?php print $messages; ?>
+    <a id="main-content"></a>
+    <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+    <?php print render($page['help']); ?>
+    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+    <?php print render($page['content']); ?>
+  </section> <!-- /#main -->
+  
+  <?php if ($page['sidebar_first']): ?>
+    <aside id="sidebar-first" role="complementary" class="sidebar clearfix">
+      <?php print render($page['sidebar_first']); ?>
+    </aside>  <!-- /#sidebar-first -->
+  <?php endif; ?>
+
+  <?php if ($page['sidebar_second']): ?>
+    <aside id="sidebar-second" role="complementary" class="sidebar clearfix">
+      <?php print render($page['sidebar_second']); ?>
+    </aside>  <!-- /#sidebar-second -->
+  <?php endif; ?>
+
+  <footer id="footer" role="contentinfo" class="clearfix">
+    <?php print render($page['footer']) ?>
+    <?php print $feed_icons ?>
+  </footer> <!-- /#footer -->
+
+</div> <!-- /#wrapper -->
